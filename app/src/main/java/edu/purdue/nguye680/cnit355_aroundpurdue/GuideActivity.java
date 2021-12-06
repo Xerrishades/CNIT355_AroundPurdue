959944a6e5c06050e843de47ad0a39bb3e5971a8
Claire Yoon <44825270+honeydoyaji@users.noreply.github.com>
2021-12-06 18:33:54 -0500

    Update GuideActivity.java

--- app/src/main/java/edu/purdue/nguye680/cnit355_aroundpurdue/GuideActivity.java
+++ app/src/main/java/edu/purdue/nguye680/cnit355_aroundpurdue/GuideActivity.java
@@ -1,27 +1,32 @@
 package edu.purdue.nguye680.cnit355_aroundpurdue;
 
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GuideActivity extends AppCompatActivity {
 
+    ImageView btnSave;
+    Location locationName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
 
-        TextView lblMain = (TextView)findViewById(R.id.lblMain);
+        TextView lblMain = (TextView)findViewById(R.id.lblSave);
         lblMain.setText(MainActivity.locations.get(MainActivity.place).name);
 
         TextView txtRate = (TextView)findViewById(R.id.txtRate);
         txtRate.setText(MainActivity.locations.get(MainActivity.place).rate);
-
-        ImageView imageView = (ImageView)findViewById(R.id.img);
-        imageView.setImageResource(MainActivity.locations.get(MainActivity.place).image);
 
         TextView txtFilter = (TextView)findViewById(R.id.txtFilter);
         txtFilter.setText(MainActivity.locations.get(MainActivity.place).filter);
@@ -43,5 +48,42 @@ public class GuideActivity extends AppCompatActivity {
 
         TextView txtRecommendation = (TextView)findViewById(R.id.txtRecommendation);
         txtRecommendation.setText(txtRecommendation.getText() + MainActivity.locations.get(MainActivity.place).recommendation);
+
+        btnSave = (ImageView) findViewById(R.id.imgUnsave);
+        locationName = MainActivity.locations.get(MainActivity.place);
+
+        if(SaveActivity.savedLocations.contains(locationName)) {
+            System.out.println("already Saved");
+            btnSave.setImageDrawable(getResources().getDrawable(R.drawable.save));
+        }
+    }
+
+    public void clickLink(View view) {
+
+       String website = MainActivity.locations.get(MainActivity.place).link;
+       Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(website));
+       startActivity(mIntent);
+    }
+
+
+    public void onClickSave(View view) {
+        if(!SaveActivity.savedLocations.contains(locationName)) {
+            System.out.println("saving");
+            btnSave.setImageDrawable(getResources().getDrawable(R.drawable.save));
+
+            SaveActivity.savedLocations.add(locationName);
+
+        } else {
+            System.out.println("unsaving");
+            btnSave.setImageDrawable(getResources().getDrawable(R.drawable.unsave));
+            SaveActivity.savedLocations.remove(locationName);
+        }
+
+        for(int i = 0; i < SaveActivity.savedLocations.size() ; i++){
+
+            System.out.println(SaveActivity.savedLocations.get(i).name);
+
+        }
+
     }
 }
